69f9f23c9bbd9eee33158162616ceb0b3d2a1d64
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-10-15 23:03:41 -0300

    Static typing com classes

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-var pessoa = {
-    nome: 'Rafael',
-    idade: 26
-};
-console.log(pessoa.nome);
+// interface Pessoa{
+//     nome: string,
+//     idade: number
+// }
+// const pessoa: Pessoa = {
+//     nome: 'Rafael',
+//     idade: 26
+// }
+// console.log(pessoa.nome)
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    Pessoa.prototype.getNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.getIdade = function () {
+        return this.idade;
+    };
+    Pessoa.prototype.setNome = function (nome) {
+        this.nome = nome;
+    };
+    Pessoa.prototype.setIdade = function (idade) {
+        this.idade = idade;
+    };
+    return Pessoa;
+}());
+var pessoa = new Pessoa('João', 29);
+var pessoa2 = new Pessoa('Rafael', 26);
+pessoa2.setNome('Felipe');
+console.log(pessoa.getNome());
+console.log(pessoa2.getNome());
--- script.ts
+++ script.ts
@@ -1,11 +1,46 @@
-interface Pessoa{
-    nome: string,
-    idade: number
+// interface Pessoa{
+//     nome: string,
+//     idade: number
+// }
+
+// const pessoa: Pessoa = {
+//     nome: 'Rafael',
+//     idade: 26
+// }
+
+// console.log(pessoa.nome)
+
+class Pessoa{
+    nome: string;
+    idade: number;
+
+    constructor(nome:string, idade:number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    getNome():string{
+        return this.nome;
+    }
+
+    getIdade():number{
+        return this.idade;
+    }
+
+    setNome(nome:string){
+        this.nome = nome;
+    }
+
+    setIdade(idade:number){
+        this.idade = idade
+    }
 }
 
-const pessoa: Pessoa = {
-    nome: 'Rafael',
-    idade: 26
-}
+let pessoa: Pessoa = new Pessoa('João', 29);
 
-console.log(pessoa.nome)
+let pessoa2 = new Pessoa('Rafael', 26)
+
+pessoa2.setNome('Felipe')
+
+console.log(pessoa.getNome())
+console.log(pessoa2.getNome())
